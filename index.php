81005defd04d96ac0918cf287483ba8608deaf4b
Bruno Oliveira <bds.commus@gmail.com>
2023-08-23 20:06:55 -0300

    inital commit

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Jogo da Velha</title>
+</head>
+<body>
+    <div class="flex justify-center h-screen">
+        <div class="grid content-center">
+            <h1 class="text-5xl">Jogo da Velha</h1>
+            <div class="flex justify-center">
+                <div class="border-solid border-2 border-sky-500 p-5 m-5">
+                <div class="flex p-3 m-3">
+                        <div class="px-3">
+                            <a href="?x=0">
+                                <div class="bg-black text-white">
+                                    guhijoiufyugh
+                                </div>
+                            </a>
+                        </div>
+                        <div class="px-3">
+                            <a href="?x=1">
+                                <div class="bg-black text-white">
+                                    guhijoiufyugh
+                                </div>
+                            </a>
+                        </div>
+                        <div class="px-3">
+                            <a href="?x=2">
+                                <div class="bg-black text-white">
+                                    guhijoiufyugh
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="flex p-3 m-3">
+                        <div class="px-3">
+                            <a href="?x=3">
+                                <div class="bg-black text-white">
+                                    guhijoiufyugh
+                                </div>
+                            </a>
+                        </div>
+                        <div class="px-3">
+                            <a href="?x=4">
+                                <div class="bg-black text-white">
+                                    guhijoiufyugh
+                                </div>
+                            </a>
+                        </div>
+                        <div class="px-3">
+                            <a href="?x=5">
+                                <div class="bg-black text-white">
+                                    guhijoiufyugh
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="flex p-3 m-3">
+                        <div class="px-3">
+                            <a href="?x=6">
+                                <div class="bg-black text-white">
+                                    guhijoiufyugh
+                                </div>
+                            </a>
+                        </div>
+                        <div class="px-3">
+                            <a href="?x=7">
+                                <div class="bg-black text-white">
+                                    guhijoiufyugh
+                                </div>
+                            </a>
+                        </div>
+                        <div class="px-3">
+                            <a href="?x=8">
+                                <div class="bg-black text-white">
+                                    guhijoiufyugh
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
