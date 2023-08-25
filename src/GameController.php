ecb5554e834d29c63b91db15315c7230b425e217
Junior Aires <126625976+JuniorAires@users.noreply.github.com>
2023-08-25 17:12:20 -0300

    Correction GameController

--- src/GameController.php
+++ src/GameController.php
@@ -1,1 +1,36 @@
-git
+<?php
+
+namespace App;
+
+class GameController
+{
+    private GameState $state;
+    private $slots = [];
+    private PlayerInterface $player;
+
+    public function __construct(ReadyState $gameState)
+    {
+        $this->state = $gameState;
+    }
+    
+    public function changeState(GameState $newState): void
+    {
+        $this->state = $newState;
+    }
+
+    public function play(PlayerInterface $player, Slot $slot): void
+    {
+        //
+    }
+
+    public function checkWinner(): void
+    {
+        //
+    }
+
+    public function changePlayer(PlayerInterface $player): void
+    {
+        //
+    }
+}
+
